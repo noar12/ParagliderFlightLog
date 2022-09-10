5e556ddb402cb8bf77386a775f7a9a6f7043d56d
Noar <arnaud.moretti@protonmail.com>
2022-09-10 13:49:01 +0200

    add a context menu to delete or edit flight delete is working (but we still have to refresh the table manualy, two way binding is not working) edit window is blank

--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
 var app = builder.Build();
 
