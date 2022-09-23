f31ce1ed287c278910f170b64f47c351baed5533
Noar <arnaud.moretti@protonmail.com>
2022-09-23 16:01:28 +0200

    fix: onAdd flight removed tmp file before importing was finished

--- ParagliderFlightLog/DataModels/FlightLogDB.cs
+++ ParagliderFlightLog/DataModels/FlightLogDB.cs
@@ -21,8 +21,8 @@ namespace ParagliderFlightLog.DataModels
         private ObservableCollection<Site> m_sites = new ObservableCollection<Site>();
         private ObservableCollection<Glider> m_gliders = new ObservableCollection<Glider>();
 
-        private const string DB_PATH = @"C:\Users\mar\source\repos\noar12\ParagliderFlightLog\ParagliderFlightLog.db";
-        //private const string DB_PATH = @"C:\Users\arnau\source\repos\ParagliderFlightLog\ParagliderFlightLog.db";
+        //private const string DB_PATH = @"C:\Users\mar\source\repos\noar12\ParagliderFlightLog\ParagliderFlightLog.db";
+        private const string DB_PATH = @"C:\Users\arnau\source\repos\ParagliderFlightLog\ParagliderFlightLog.db";
         //private const string DB_PATH = @"/home/noar/TestDB/ParagliderFlightLog.db";
 
         public FlightLogDB()
