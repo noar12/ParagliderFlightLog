0a41a78115429bd1165bcd75049fed924d129af9
Arnaud Moretti <arnaud.moretti@matisa.ch>
2024-02-19 10:53:20 +0100

    add identity pages to project to register/login/logout authorrize to see the app only for registred user

--- ParaglidingFlightLogWeb/ParaglidingFlightLogWeb/Program.cs
+++ ParaglidingFlightLogWeb/ParaglidingFlightLogWeb/Program.cs
@@ -28,9 +28,9 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("UserDataSqlite") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
