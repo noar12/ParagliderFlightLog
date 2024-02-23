1fcd0f009374f3763177fa6e00ea46fa128bfc8b
Arnaud Moretti <arnaud.moretti@matisa.ch>
2024-02-23 08:39:59 +0100

    getting user info in index

--- ParaglidingFlightLogWeb/ParaglidingFlightLogWeb/Components/Pages/Index.razor.cs
+++ ParaglidingFlightLogWeb/ParaglidingFlightLogWeb/Components/Pages/Index.razor.cs
@@ -3,10 +3,34 @@ using Microsoft.AspNetCore.Components.Web;
 using Radzen;
 using Radzen.Blazor;
 using ParaglidingFlightLogWeb.ViewModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using ParaglidingFlightLogWeb.Data;
 
-namespace ParaglidingFlightLogWeb.Components.Pages
+namespace ParaglidingFlightLogWeb.Components.Pages;
+
+public partial class Index
 {
-    public partial class Index
+    [CascadingParameter]
+    private Task<AuthenticationState> AuthenticationStateTask { get; set; } = null!;
+    [Inject]
+    UserManager<ApplicationUser> UserManager { get; set; } = null!;
+
+    string? userName;
+    string? userId;
+    string? userEmail;
+
+    protected override async Task OnInitializedAsync()
     {
+        var userClaim = (await AuthenticationStateTask).User;
+        if (userClaim.Identity is not null && userClaim.Identity.IsAuthenticated)
+        {
+            var currentUser = await UserManager.GetUserAsync(userClaim);
+            if (currentUser == null) return;
+            userName = currentUser.UserName;
+            userId = currentUser.Id;
+            userEmail = currentUser.Email;
+        }
     }
 }
