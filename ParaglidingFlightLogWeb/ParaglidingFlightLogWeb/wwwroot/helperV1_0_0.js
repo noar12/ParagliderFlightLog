353818002e2dca8d3f3d761659e9496560d1a2b5
noar <piers.son@protonmail.com>
2026-04-13 16:57:18 +0200

    create a button to export a flight to the clipboard as a json

---
+++ ParaglidingFlightLogWeb/ParaglidingFlightLogWeb/wwwroot/helperV1_0_0.js
@@ -0,0 +1,7 @@
+export function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Text copied to clipboard');
+    }).catch(function(error) {
+        console.error('Error copying text: ', error);
+    });
+}
